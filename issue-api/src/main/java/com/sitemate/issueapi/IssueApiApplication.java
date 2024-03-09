a460219050e545dd61706f78edc910bf4a2ad5e1
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2024-03-09 17:31:34 -0300

    server setup

---
+++ issue-api/src/main/java/com/sitemate/issueapi/IssueApiApplication.java
@@ -0,0 +1,13 @@
+package com.sitemate.issueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueApiApplication.class, args);
+	}
+
+}
---
+++ issue-api/src/test/java/com/sitemate/issueapi/IssueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitemate.issueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
